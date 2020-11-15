e4a39fbbccb251198428b4e20f46780ac6523cbd
Nagesh KR <krnagesh@hotmail.com>
2020-11-15 11:23:16 +0530

    placing all nuxt related files here

---
+++ components/NavBar.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="navbar">
+    <section class="navbar-section">
+      <nuxt-link :to="{ name: 'index' }" class="avbar-brand mr-2">
+        <h1>Nagesh</h1>
+      </nuxt-link>
+      <!-- <nuxt-link class="btn btn-link" :to="{ name: 'post' }"> POST </nuxt-link> -->
+      <!-- <a href="..." class="btn btn-link">Docs</a>
+      <a href="..." class="btn btn-link">GitHub</a> -->
+    </section>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
+
+<style>
+.avbar-brand {
+  color: #06b0f1;
+}
+
+a:visited {
+  color: #06b0f1;
+}
+
+a:hover {
+  color: #06b0f1;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hero hero-lg">
+    <div class="hero-body">
+      <div class="columns">
+        <div class="column col-3 col-mx-auto">
+          <h3>Hey there!</h3>
+          <p>I'm working on my profile and will update the site soon!!!</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #344359;
+  color: #fdfdfe;
+}
+</style>
